0ed79b91074993c1fa98e3f69e901be727c6fb07
Edna Vidal <ednavidalros@gmail.com>
2023-08-02 18:25:02 +0200

    "First commit"

--- app.js
+++ app.js
@@ -2,24 +2,39 @@ const express = require('express');
 
 const hbs = require('hbs');
 const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
-const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-// ...
-
-// Add the route handlers here:
+app.set("view engine", "hbs")
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/about", (req, res) => {
+  res.render("about");
+})
+
+app.get("/beers", (req, res) => {
+  fetch("https://api.punkapi.com/v2/beers")
+  .then((response) => response.json())
+  .then((beers) => {
+    res.render("beers", {beers})
+  })
+})
+
+app.get("/random-beer", (req, res) => {
+  fetch("https://api.punkapi.com/v2/beers/random")
+  .then((response) => response.json())
+  .then((randomBeer) => {
+    res.render("random-beer", {randomBeer})
+  })
+})
+
+app.listen(3000);
